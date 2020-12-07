846014e721d55ac865d5bbfa69aa4df39635664e
Lou-AnnBoinet <lou-ann.boinet@efrei.net>
2020-12-07 18:37:27 +0100

    implémentation fonctions code huffman partie 2

--- fonctions_2.h
+++ fonctions_2.h
@@ -1,29 +1,43 @@
 #ifndef FONCTION_H_INCLUDED
 #define FONCTION_H_INCLUDED
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+#define TAILLE 50
 
 typedef struct Noeud{
-    int info; // L'information d'un noeud ou d'une feuille = sa valeur
-    struct Noeud* sag; // Pour aller au prochain élément côté gauche
-    struct Noeud* sad; // Pour aller au prochain élément côté droit
+    int info;
+    char caractere; 
+    struct Noeud* sag; 
+    struct Noeud* sad; 
 } Noeud;
 
+typedef Noeud* Arbre;
+
+
 typedef struct Element{
-    int occurrence;
+    int occurence;
     char caractere;
+    Arbre arbre;
     struct Element* suiv;
 }Element;
 
 typedef Element* Liste;
 
 Element* Creer_New_Element(char c);
-Element* Liste_occurrence_caractere(char* input, Element* Liste);
+Element* Liste_occurence_caractere(char* input);
 void afficher_liste (Element* Liste);
-void liberer_Liste(Element* Liste);
+int liberer_Liste(Element* Liste);
 int Compare_caractere(Element* Liste, char carac);
-int minimum_Liste(Element* Liste);
+
+
+Noeud* creer_noeud(int valeur);
+Noeud* creer_arbre(int n);
+
+
+void creation_Dictionnaire(Arbre A,char *fichierDictionnaire);
+
+Arbre creer_Arbre_Huffman(Element* Liste);
+void traduire_binaire_dico_Huffman (char* fichierdico,char *dictionnaire[256],Arbre *arbre);
+void codage_fichier(char* fichierinput,char* fichieroutput,char *dictionnaire[256]);
+void decodage_fichier(char* fichierinput,char* fichieroutput,Arbre arbre);
+
 
 #endif
