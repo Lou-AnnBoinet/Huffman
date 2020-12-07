d256b8e20f62aabf4fbe765ce2a01a519cf19b01
maelysvery <maelys.very@efrei.net>
2020-12-07 18:53:30 +0100

    implementation fonctions binaire partie 1

--- fonctions.c
+++ fonctions.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define TAILLE_MAX 150000 // On définit une taille maximale à notre tableau
-#include "fonction.h"
+#include "fonctions.h"
+
+
+//partie 1
 
 void convertir_binaire(char caractere, char* conteneur)
 {
@@ -31,49 +33,3 @@ void traduire_binaire_texte(char* input, char* traduction)
     char conteneur[9] ;
     while ( (temp = getc(texte)) != EOF)
     {
-        convertir_binaire(temp, conteneur);
-        fprintf(texte2, "%s" , conteneur);
-    }
-    fclose(texte2);
-    fclose(texte);
-}
-/*______________________________________________*/
-/*______________________________________________*/
-/*______________________________________________*/
-
-int nombre_caracteres()
-{
-    FILE *fichier = NULL; // Pointeur initialisé à NULL
-    int cmpt = 0; // Un compteur initialisé à 0
-    char *tab = (char*)malloc(sizeof(char)*TAILLE_MAX); // On alloue de la mémoire à un tableau
-    // On met deux \\ pour le fichier afin d'utiliser la méthode du chemin absolu
-    fichier = fopen("input.txt", "r"); // Le pointeur fichier devient un pointeur sur input
-    if (fichier == NULL)
-    {
-        return -1; // Fichier vide
-    }
-    else{
-        while (fgets(tab, TAILLE_MAX, fichier) != NULL) // La fonction fgets lit les caracteres du fichier, les range dans notre tableau
-        {
-            cmpt = cmpt + strlen(tab); // strlen --> permet de calculer la longueur d'une chaîne de caracteres
-        }
-    }
-    fclose(fichier); // Fermer le fichier une fois qu'on l'a utilisé
-    return cmpt;
-}
-
-int nombre_en_binaire()
-{
-    FILE *fichier = NULL;
-    int cmpt = 0;
-    char *tab = (char*)malloc(sizeof(char)*TAILLE_MAX);
-    fichier = fopen("traduction.txt", "r");
-    if (fichier == NULL) return -1;
-    else{
-        while (fgets(tab, TAILLE_MAX, fichier) != NULL){
-            cmpt = cmpt + strlen(tab);
-        }
-    }
-    fclose(fichier);
-    return cmpt;
-}
--- fonctions.h
+++ fonctions.h
@@ -1,14 +1,12 @@
 #ifndef FONCTION_H_INCLUDED
 #define FONCTION_H_INCLUDED
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+#define TAILLE_MAX 150000 // taille maximale tableau
+
 
 void convertir_binaire(char caractere, char* conteneur);
 void traduire_binaire_texte(char* input, char* traduction);
-int nombre_caracteres();
-int nombre_en_binaire();
+
+int nombre_caracteres(char* nomfichier);
+
 
 #endif
-#define TAILLE_MAX 150000 // On définit une taille maximale à notre tableau
